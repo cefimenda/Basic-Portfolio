94527005ea8bb247c6d41fd5b835d69d6265a980
cefimenda <jeffymenda@gmail.com>
2018-06-13 12:20:58 -0400

    made edits to content. Assignment now ready for submission.

--- assets/javascript.js
+++ assets/javascript.js
@@ -3,7 +3,7 @@ $(function(){
     $("button").click(function(){
         var buttonName = $(this).html().toLowerCase()
         if(buttonName == "submit"){
-            alert("Thank you for your submission! I will get in contact with you shortly.");
+            alert("Thank you for your message! I will get in contact with you shortly.");
             buttonName = "index";
         }
         if (buttonName == "about"){buttonName = "index"}
